3778a260a7df51c41f5a88cd73c755ef5c42de5a
c24012 <c24012@std.it-college.ac.jp>
2025-05-22 14:27:13 +0900

    P50-P68 第4章演習 終了

---
+++ Chapter04/ex4-13.py
@@ -0,0 +1,5 @@
+def odd_if(x,y = 10):
+    return x + y
+
+result = odd_if(2)
+print(result)
---
+++ Chapter04/ex4-23.py
@@ -0,0 +1,9 @@
+def add(x,y):
+    """
+    Returns x + y.
+    :param x:i int.
+    :param y:int.
+    :return:int sum of x and y.
+    """
+
+    return x + y
