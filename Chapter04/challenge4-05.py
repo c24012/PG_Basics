fcb560c6ea838919032bf31fe45e332d2eb4c8ea
c24012 <c24012@std.it-college.ac.jp>
2025-05-22 16:30:14 +0900

    チャレンジ問題修正&改良_2

--- Chapter04/challenge4-05.py
+++ Chapter04/challenge4-05.py
@@ -1,7 +1,19 @@
-s = input("数字を入力してください:")
+def str_to_flo(s):
+    """
+    引数s(string)をfloatに変換する関数
 
-try:
-    f = float(s)
-    print(f)
-except ValueError:
-    print("数字が入力されていません")
+    引数:
+        s : string
+    戻り値:
+        f : float
+    """
+
+    try:
+        f = float(s)
+        return f
+    except ValueError:
+        print("数字が入力されていません")
+
+#main
+s = input("数字を入力してください:")
+print(str_to_flo(s))
